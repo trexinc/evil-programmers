50690c35dc6b44a940d43eff45c30e667e7697f2
yaroslavsky_a <yaroslavsky_a@73dea132-4c3a-11de-8d8d-752d95cf3e3c>
2006-07-15 13:58:52 +0000

    add CharacterMap

---
+++ trunk/CharacterMap/src/registry.hpp
@@ -0,0 +1,14 @@
+#ifndef __REGISTRY_HPP__
+#define __REGISTRY_HPP__
+#include <windows.h>
+
+/*
+int GetRegKey(const char *ValueName,char *ValueData,char *Default,DWORD DataSize);
+void SetRegKey(const char *ValueName,char *ValueData);
+void DelRegValue(char* ValueName);
+*/
+void SetRegKey(const char *ValueName,DWORD ValueData);
+BOOL GetRegKey(const char *ValueName,int *ValueData,DWORD Default);
+extern char PluginRootKey[];
+
+#endif
