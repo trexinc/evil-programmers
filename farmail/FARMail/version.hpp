4f947144e7a73f17293d2843397c45b29bd08ad8
z-g <oo.zg.oo@gmail.com>
2007-12-14 13:10:34 +0000

    AddressBook build fixed.

--- farmail/FARMail/version.hpp
+++ farmail/FARMail/version.hpp
@@ -1,13 +1,13 @@
 #define VER_HI "3"
 #define VER_LO "0"
 #define BUILD "91"
-#define BUILD_YEAR "2005"
+#define BUILD_YEAR "2007"
 #define BUILD_MONTH "12"
-#define BUILD_DAY "09"
+#define BUILD_DAY "14"
 #ifdef FARMAIL_SSL
 #define COPYRIGHT1 "FARMail v%s.%s (build %s) with SSL support, compiled on %s.%s.%s."
 #else
 #define COPYRIGHT1 "FARMail v%s.%s (build %s), compiled on %s.%s.%s."
 #endif
-#define COPYRIGHT2 "Copyright (C) 2002-2005 FARMail Group."
+#define COPYRIGHT2 "Copyright (C) 2002-2007 FARMail Group."
 #define COPYRIGHT3 "Copyright (C) 1999-2000 Serge Alexandrov."
