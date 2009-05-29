58d1d5872cc7a3dce397e93ecf2f2ba7969b0d48
trexinc <trexinc@73dea132-4c3a-11de-8d8d-752d95cf3e3c>
2009-05-29 11:43:13 +0000

    remove unneeded tag

--- tags/start/farmail/Helper/memory.h
+++
@@ -1,3 +0,0 @@
-#define malloc(size) HeapAlloc(GetProcessHeap(),HEAP_ZERO_MEMORY,size)
-#define free(ptr) ((ptr)?HeapFree(GetProcessHeap(),0,ptr):0)
-#define realloc(ptr,size) ((size)?((ptr)?HeapReAlloc(GetProcessHeap(),HEAP_ZERO_MEMORY,ptr,size):HeapAlloc(GetProcessHeap(),HEAP_ZERO_MEMORY,size)):(HeapFree(GetProcessHeap(),0,ptr),(void *)0))
