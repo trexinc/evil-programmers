962a9803d5c89afbb4ea37aeda0f29624a3877c1
trexinc <trexinc@73dea132-4c3a-11de-8d8d-752d95cf3e3c>
2011-12-27 11:13:51 +0000

    use far makes, VC support, 64 bit support

--- trunk/Visualizer/src/Visualizer.hpp
+++ trunk/Visualizer/src/Visualizer.hpp
@@ -16,8 +16,8 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-#include "plugin.hpp"
 #include <CRT/crt.hpp>
+#include "plugin.hpp"
 
 extern struct PluginStartupInfo Info;
 extern FARSTANDARDFUNCTIONS FSF;
