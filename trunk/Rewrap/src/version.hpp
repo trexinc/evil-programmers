2311cdedb65bc9264e560247d44a190ede5660bf
trexinc <trexinc@73dea132-4c3a-11de-8d8d-752d95cf3e3c>
2011-12-27 10:53:55 +0000

    use far makes, VC support, 64 bit support

---
+++ trunk/Rewrap/src/version.hpp
@@ -0,0 +1,7 @@
+#define PLUGIN_MAJOR 2
+#define PLUGIN_MINOR 1
+#define PLUGIN_DESC L"Reformat paragraph based on ESC plugin settings"
+#define PLUGIN_NAME L"Rewrap"
+#define PLUGIN_FILENAME L"rewrap.dll"
+#define PLUGIN_AUTHOR L"Stanislav V. Mekhanoshin & Alex Yaroslavsky"
+#define PLUGIN_VERSION MAKEFARVERSION(PLUGIN_MAJOR,PLUGIN_MINOR,0,0,VS_RELEASE)
