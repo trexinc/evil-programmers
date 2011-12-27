0e063934002aa23c10d786ebde85aff1480f5a9f
trexinc <trexinc@73dea132-4c3a-11de-8d8d-752d95cf3e3c>
2011-12-27 07:18:57 +0000

    use far makes, VC support, 64 bit support

---
+++ trunk/ESC-TSC-Mini/src/version.hpp
@@ -0,0 +1,7 @@
+#define PLUGIN_MAJOR 1
+#define PLUGIN_MINOR 3
+#define PLUGIN_DESC L"ESC's Temporary Settings Changer - Minimalistic"
+#define PLUGIN_NAME L"ESC-TSC-Mini"
+#define PLUGIN_FILENAME L"esc-tsc-mini.dll"
+#define PLUGIN_AUTHOR L"Alex Yaroslavsky"
+#define PLUGIN_VERSION MAKEFARVERSION(PLUGIN_MAJOR,PLUGIN_MINOR,0,0,VS_RELEASE)
