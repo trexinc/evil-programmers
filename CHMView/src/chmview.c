4802a55823c9b734574652d4eed0bc0b4bebdc8e
trexinc <trexinc@gmail.com>
2012-02-19 15:10:41 +0000

    Unix support from Alex Kozlov

--- CHMView/src/chmview.c
+++ CHMView/src/chmview.c
@@ -1,6 +1,6 @@
 /*
     CHMView - list and extract files from CHM/CHI HtmlHelp archives
-    Copyright (C) 2002-2005 Alex Yaroslavsky
+    Copyright (C) 2002-2012 Alex Yaroslavsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -17,19 +17,53 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#ifdef UNIX
+#include <sys/stat.h>
+#include <unistd.h>
+#else
 #include <windows.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
-#include <dir.h>
 
+#ifdef UNIX
+#include <errno.h>
+#include <locale.h>
+#else
+#include <dir.h>
+#endif
+
+#ifdef UNIX
+#include <chm_lib.h>
+#else
 #include "chm_lib.h"
+#endif
+
 #include "utf8.h"
+
+#ifdef UNIX
+#define _strnicmp strncasecmp
+#define MODE 0755
+#endif
+
+char *pointtoname(char *path);
+void ReplaceIllegalChars(char *ptr);
+void extract(struct chmFile *c, struct chmUnitInfo *ui,
+		long extractwithoutpath);
+int _extract_callback_all(struct chmFile *c, struct chmUnitInfo *ui,
+		void *context);
+int _extract_callback_dir(struct chmFile *c, struct chmUnitInfo *ui,
+		void *context);
+int extractdir(struct chmFile *c, char *path, long extractwithoutpath);
+int _print_ui(struct chmFile *c __unused, struct chmUnitInfo *ui,
+		void *context __unused);
 
 struct cb_data
 {
-  int extractwithoutpath;
+  long extractwithoutpath;
   char *path;
   int pathlen;
 };
@@ -44,6 +78,7 @@ char *pointtoname(char *path)
   return p;
 }
 
+#ifndef UNIX
 int makedir(char *path)
 {
   char *end;
@@ -75,13 +110,45 @@ int makedir(char *path)
   }
   return 1;
 }
+#else
+static int makedir(char *path)
+{
+  char *slash;
+  int  was_error, done;
+
+  slash = path;
+  done = 0;
+  was_error = 0;
+
+  do {
+    slash = index(slash + 1, '/');
+    if (slash)
+    {
+      *slash = 0;
+      if (mkdir(path, MODE) == -1 && EEXIST != errno)
+		was_error = 1;
+      *slash = '/';
+    }
+    else
+       done = 1;
+  } while (! done && ! was_error);
+
+  if (was_error)
+  {
+    perror("mkdir");
+	exit(1);
+    /* return 0; */
+  }
+
+  return 1;
+}
+#endif
 
 const char ILLEGAL_SYMB[] = "<>:|?*\"";
 const char ILLEGAL_REPL[] = "()_!__'";
 
 void ReplaceIllegalChars(char *ptr)
 {
-  char *org = ptr;
   while ( *ptr )
   {
     const char *zz;
@@ -94,15 +161,17 @@ void ReplaceIllegalChars(char *ptr)
   }
 }
 
-static int savetofile(struct chmFile *c, struct chmUnitInfo *ui, int extractwithoutpath)
+static int savetofile(struct chmFile *c, struct chmUnitInfo *ui, long extractwithoutpath)
 {
   LONGINT64 length=0;
   char *outbuf=NULL;
-  char *p;
   FILE *f;
   char fullpath[CHM_MAX_PATHLEN*2+1];
   char target[CHM_MAX_PATHLEN*2+1];
   wchar_t temp[CHM_MAX_PATHLEN+1];
+#ifdef UNIX
+  size_t pathlen;
+#endif
 
   if (ui->length)
   {
@@ -116,13 +185,32 @@ static int savetofile(struct chmFile *c, struct chmUnitInfo *ui, int extractwith
       return -1;
     }
   }
+#ifndef UNIX
   GetCurrentDirectory(sizeof(fullpath)-1,fullpath);
   while ((p = strstr(fullpath,"\\")) != NULL)
     *p = '/';
   if (fullpath[strlen(fullpath)-1] != '/')
     strcat(fullpath,"/");
+#else
+  getcwd(fullpath, sizeof(fullpath)-1);
+  pathlen = strlen(fullpath);
+  if (pathlen == 0)
+	return -1;	/* impossible but... defensive programming */
+  if (fullpath[pathlen - 1] != '/')
+  {
+    if (pathlen == PATH_MAX)
+		return -1;
+    else
+		fullpath[pathlen] = '/';
+	fullpath[pathlen+1] = 0;
+  }
+#endif
   decode_UTF8(temp,ui->path);
+#ifndef UNIX
   WideCharToMultiByte(CP_ACP,0,temp,-1,target,sizeof(target),NULL,NULL);
+#else
+  wcstombs(target, temp, sizeof(target));
+#endif
   ReplaceIllegalChars(target);
   strcat(fullpath,extractwithoutpath?pointtoname(target):(target[0]=='/'?target+1:target));
   if (!extractwithoutpath)
@@ -147,30 +235,38 @@ static int savetofile(struct chmFile *c, struct chmUnitInfo *ui, int extractwith
   return 0;
 }
 
-void extract(struct chmFile *c, struct chmUnitInfo *ui, int extractwithoutpath)
+void extract(struct chmFile *c, struct chmUnitInfo *ui, long extractwithoutpath)
 {
   char target[CHM_MAX_PATHLEN*2+1];
   wchar_t temp[CHM_MAX_PATHLEN+1];
 
   decode_UTF8(temp,ui->path);
+#ifndef UNIX
   WideCharToMultiByte(CP_OEMCP,0,temp,-1,target,sizeof(target),NULL,NULL);
+#else
+  wcstombs(target, temp, sizeof(target));
+#endif
   printf("Extracting %s ",target);
   if (savetofile(c, ui, extractwithoutpath))
   {
     printf("Error\n");
     error=1;
     //return CHM_ENUMERATOR_FAILURE;
-  }
+  } else
   printf("OK\n");
 }
 
-int _extract_callback_all(struct chmFile *c, struct chmUnitInfo *ui, void *context)
+int _extract_callback_all(struct chmFile *c,
+		struct chmUnitInfo *ui,
+		void *context)
 {
-  extract(c,ui,(int)context);
+  extract(c,ui,(long)context);
   return CHM_ENUMERATOR_CONTINUE;
 }
 
-int _extract_callback_dir(struct chmFile *c, struct chmUnitInfo *ui, void *context)
+int _extract_callback_dir(struct chmFile *c,
+		struct chmUnitInfo *ui,
+		void *context)
 {
   struct cb_data *data = (struct cb_data *)context;
   if (!_strnicmp(ui->path,data->path,data->pathlen))
@@ -180,29 +276,35 @@ int _extract_callback_dir(struct chmFile *c, struct chmUnitInfo *ui, void *conte
   return CHM_ENUMERATOR_CONTINUE;
 }
 
-extractdir(struct chmFile *c, char *path, int extractwithoutpath)
+int extractdir(struct chmFile *c, char *path, long extractwithoutpath)
 {
   struct cb_data data = {extractwithoutpath,path,strlen(path)};
   chm_enumerate(c,CHM_ENUMERATE_ALL,_extract_callback_dir,(void *)&data);
   return 0;
 }
 
-int _print_ui(struct chmFile *c, struct chmUnitInfo *ui, void *context)
+int _print_ui(struct chmFile *c __unused,
+		struct chmUnitInfo *ui,
+		void *context __unused)
 {
   char target[CHM_MAX_PATHLEN*2+1];
   wchar_t temp[CHM_MAX_PATHLEN+1];
 
   decode_UTF8(temp,ui->path);
+#ifndef UNIX
   WideCharToMultiByte(CP_OEMCP,0,temp,-1,target,sizeof(target),NULL,NULL);
+#else
+  wcstombs(target, temp, sizeof(target));
+#endif
 
   if ((target)[0] == '/')
   {
-    printf("%12.12u ",ui->length);
+    printf("%12.12llu ",ui->length);
     printf("%s\n",target);
   }
   else
   {
-    printf("%12.12u ",ui->length);
+    printf("%12.12llu ",ui->length);
     printf("/");
     printf("%s\n",target);
   }
@@ -211,7 +313,7 @@ int _print_ui(struct chmFile *c, struct chmUnitInfo *ui, void *context)
 
 static void usage(void)
 {
-  printf("CHMView v2.0 beta 3 with UTF-8 support (March 7 2005)\n");
+  printf("CHMView v2.0 beta 4 with UTF-8 support (February 19 2012)\n");
   printf("(c) Alex Yaroslavsky at yandex / trexinc.\n");
   printf("Using:\nCHMLib library by Jed Wing at ugcs.caltech.edu / jedwin\n");
   printf("LZX library by Stuart Caie at 4u.net / kyzer\n");
@@ -231,8 +333,6 @@ int main(int argc, char *argv[])
   struct chmFile *c;
   char *infname;
   char *command;
-  FILE *fh;
-  int i;
   struct chmUnitInfo ui;
 
   if (argc < 3)
@@ -243,6 +343,9 @@ int main(int argc, char *argv[])
 
   infname = argv[2];
 
+#ifdef UNIX
+  setlocale(LC_ALL,"");
+#endif
   c = chm_open(infname);
   if (!c)
     exit(-1);
@@ -265,7 +368,7 @@ int main(int argc, char *argv[])
       char target[CHM_MAX_PATHLEN*2+1];
       wchar_t temp[CHM_MAX_PATHLEN+1];
       int status;
-      int extractwithoutpath = command[0]=='e'?1:0;
+      long extractwithoutpath = command[0]=='e'?1:0;
 
       if (argc == 4)
       {
@@ -305,7 +408,11 @@ int main(int argc, char *argv[])
             strcat(target,name+1);
           else
             strcat(target,name);
+#ifndef UNIX 
           MultiByteToWideChar(CP_ACP,0,target,-1,temp,sizeof(temp));
+#else
+		  mbstowcs(temp, target, sizeof(temp));
+#endif
           encode_UTF8(target,temp);
           status = chm_resolve_object(c,target,&ui);
           if (status==CHM_RESOLVE_SUCCESS && ui.path[strlen(ui.path)-1]!='/')
