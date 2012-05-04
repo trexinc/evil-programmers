39f7162b5c05b7ccb927119de4f54a591fcedfd4
trexinc <trexinc@gmail.com>
2012-05-04 23:19:31 +0000

    Far 3.0 build 2572 support.

--- Crapculator/src/version.hpp
+++ Crapculator/src/version.hpp
@@ -1,5 +1,5 @@
 #define PLUGIN_MAJOR 0
-#define PLUGIN_MINOR 8
+#define PLUGIN_MINOR 9
 #define PLUGIN_DESC L"Crapculates current line in editor"
 #define PLUGIN_NAME L"Crapculator & Hackulator"
 #define PLUGIN_FILENAME L"Crapculator.dll"
