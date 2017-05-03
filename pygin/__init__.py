36d5bc476349bf167a2a22ec490431ffe03f35c3
anatoly techtonik <techtonik@gmail.com>
2017-05-03 13:53:46 +0300

    Do not keep log file open
    This allows to remove/clean the log

--- pygin/__init__.py
+++ pygin/__init__.py
@@ -5,15 +5,16 @@ import tempfile
 class Logger(object):
     def __init__(self):
         self.terminal = sys.stdout
-        self.log = open(os.path.join(tempfile.gettempdir(), "pygin.log"), "a")
+        self.logfile = os.path.join(tempfile.gettempdir(), "pygin.log")
 
     def write(self, message):
+        with open(self.logfile, "a") as log:
+            log.write(message)
         self.terminal.write(message)
-        self.log.write(message)
         self.flush()
 
     def flush(self):
-        self.log.flush()
+        self.terminal.flush()
 
 Log = Logger()
 sys.stdout = Log
