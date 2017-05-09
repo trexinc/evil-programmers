89f18683e63e40e5d99992761559519b79b7b30b
Alex Alabuzhev <alabuzhev@gmail.com>
2017-05-09 21:59:31 +0100

    pygin: refactoring

--- pygin/far_api.cpp
+++ pygin/far_api.cpp
@@ -7,17 +7,27 @@
 
 namespace far_api_implementation
 {
-	static const char ApiPointerAttribute[] = "api_pointer";
+	static const char ApiPointerAttributeName[] = "api_pointer";
 
-	static const far_api* Api(PyObject* Object)
+	static const auto& api(PyObject* Object)
 	{
-		const auto Ptr = py::object::from_borrowed(Object).get_attribute(ApiPointerAttribute);
-		return reinterpret_cast<const far_api*>(PyLong_AsSize_t(Ptr.get()));
+		const auto Ptr = py::object::from_borrowed(Object).get_attribute(ApiPointerAttributeName);
+		return *reinterpret_cast<const far_api*>(PyLong_AsSize_t(Ptr.get()));
+	}
+
+	static const auto& fsf(PyObject* Object)
+	{
+		return api(Object).fsf();
+	}
+
+	static const auto& psi(PyObject* Object)
+	{
+		return api(Object).psi();
 	}
 
 	static PyObject* GetUserScreen(PyObject* self, PyObject* args)
 	{
-		Api(self)->m_Psi.PanelControl(PANEL_NONE, FCTL_GETUSERSCREEN, 0, nullptr);
+		psi(self).PanelControl(PANEL_NONE, FCTL_GETUSERSCREEN, 0, nullptr);
 
 		Py_INCREF(Py_None);
 		return Py_None;
@@ -25,7 +35,7 @@ namespace far_api_implementation
 
 	static PyObject* SetUserScreen(PyObject* self, PyObject* args)
 	{
-		Api(self)->m_Psi.PanelControl(PANEL_NONE, FCTL_SETUSERSCREEN, 0, nullptr);
+		psi(self).PanelControl(PANEL_NONE, FCTL_SETUSERSCREEN, 0, nullptr);
 
 		Py_INCREF(Py_None);
 		return Py_None;
@@ -36,18 +46,10 @@ namespace far_api_implementation
 #define FUNC_NAME_VALUE(x) #x, x
 		{ FUNC_NAME_VALUE(GetUserScreen), METH_NOARGS, "Get user screen" },
 		{ FUNC_NAME_VALUE(SetUserScreen), METH_NOARGS, "Set user screen" },
+
 #undef FUNC_NAME_VALUE
 		{}
 	};
-}
-
-far_api::far_api(const PluginStartupInfo* Psi):
-	m_Module(nullptr),
-	m_Exception(nullptr),
-	m_Psi(*Psi),
-	m_Fsf(*Psi->FSF)
-{
-	m_Psi.FSF = &m_Fsf;
 
 	static PyModuleDef ModuleDefinition =
 	{
@@ -55,22 +57,38 @@ far_api::far_api(const PluginStartupInfo* Psi):
 		"farapi",
 		"Far API module",
 		-1,
-		far_api_implementation::methods
+		methods
 	};
+}
 
-	m_Module = py::object(PyModule_Create(&ModuleDefinition));
+far_api::far_api(const PluginStartupInfo* Psi):
+	m_Module(PyModule_Create(&far_api_implementation::ModuleDefinition)),
+	m_Exception(PyErr_NewException("farapi.error", nullptr, nullptr)),
+	m_Psi(*Psi),
+	m_Fsf(*Psi->FSF)
+{
+	m_Psi.FSF = &m_Fsf;
+
 	if (!m_Module)
-		return;
+		throw std::runtime_error("PyModule_Create");
 
-	m_Exception = py::object(PyErr_NewException("spam.error", nullptr, nullptr));
 	DONT_STEAL_REFERENCE(m_Exception.get());
 	PyModule_AddObject(m_Module.get(), "error", m_Exception.get());
 
-	m_Module.set_attribute(far_api_implementation::ApiPointerAttribute, py::integer(reinterpret_cast<uintptr_t>(this)));
+	m_Module.set_attribute(far_api_implementation::ApiPointerAttributeName, py::integer(reinterpret_cast<uintptr_t>(this)));
 }
 
 far_api::~far_api()
 {
+}
+
+const PluginStartupInfo& far_api::psi() const
+{
+	return m_Psi;
+}
+const FarStandardFunctions& far_api::fsf() const
+{
+	return m_Fsf;
 }
 
 const py::object& far_api::get() const
--- pygin/far_api.hpp
+++ pygin/far_api.hpp
@@ -2,23 +2,21 @@
 
 #include "py_object.hpp"
 
-struct PyModuleDef;
-
 class far_api
 {
 public:
 	NONCOPYABLE(far_api);
 
-	far_api(const PluginStartupInfo* Psi);
+	explicit far_api(const PluginStartupInfo* Psi);
 	~far_api();
 
+	const PluginStartupInfo& psi() const;
+	const FarStandardFunctions& fsf() const;
 	const py::object& get() const;
 
 private:
 	py::object m_Module;
 	py::object m_Exception;
-
-public: // BUGBUG
 	PluginStartupInfo m_Psi;
 	FarStandardFunctions m_Fsf;
 };
